2dc3b9fadea6ea64e08a4cad61281db889ddbbcc
DG4Z <rogerio.aparecido.prestserv@petrobras.com.br>
2022-09-16 12:50:12 -0300

    Iniciando Decorator - Relembrando Patterns

---
+++ Decorator/Interfaces/IPizza.cs
@@ -0,0 +1,7 @@
+﻿namespace Decorator.Interfaces
+{
+    public interface IPizza
+    {
+        string BuscarTipo();
+    }
+}
---
+++ Decorator/Models/Pizza.cs
@@ -0,0 +1,17 @@
+﻿using Decorator.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Decorator.Models
+{
+    public class Pizza : IPizza
+    {
+        public string BuscarTipo()
+        {
+            var pizza = "tipo pizza";
+
+            return pizza;
+        }
+    }
+}
